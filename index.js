23fbb450784477830181c9422a830a47b0cf7591
Cihsan <mutamuty@gmail.com>
2022-05-26 19:16:08 +0600

    added some api

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ app.get('/', (req, res) => {
     res.send('Welcome To Manufacture Server')
 })
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.VALID_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lnkho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,14 +35,27 @@ async function run() {
         const test = client.db("testDB").collection("testcollection");
         const reviews = client.db("reviewDB").collection("reviewcollection");
         const userprifile = client.db("prifileDB").collection("prifileCollection");
-        
-        app.post('/review-post', async (req, res) => {
-            const newPD = req.body
-            const result = await reviews.insertOne(newPD);
-            res.send({ success: 'Added Product Successfully' })
-            
-        }) 
+        const userlogin = client.db("userLoginDB").collection("userLoginCollection");
+        const product = client.db("productDB").collection("productCollection");
 
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userprifile.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+        }
+        //user entry
+    
+        app.post('/review-post', verifyJWT, async (req, res) => {
+            const review = req.body
+            const result = await reviews.insertOne(review);
+            res.send({ success: 'Added review Successfully' })
+        })
+        
         //get
         app.get('/review-get', async (req, res) => {
             const query = {}
@@ -37,24 +64,39 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/profile-get', async (req, res) => {
+        /* app.get('/profile-get', async (req, res) => {
             const query = {}
             const cursor = userprifile.find(query)
             const result = await cursor.toArray()
             res.send(result)
-        })
-        
-        app.post('/profile-post', async (req, res) => {
+        }) */
+        //
+        app.get('/profile-get', verifyJWT, async (req, res) => {
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+              const query = { email: email };
+              const bookings = await userprifile.find(query).toArray();
+              return res.send(bookings);
+            }
+            else {
+              return res.status(403).send({ message: 'forbidden access' });
+            }
+          });
+
+        app.post('/profile-post', verifyJWT, async (req, res) => {
             const submit = req.body;
             const query = {
-                date: submit.date, 
-                phone: submit.phone, 
-                study: submit.study, 
-                country: submit.country, 
-                state: submit.state, 
-                zip: submit.zip, 
+                name: submit.name,
+                email: submit.email,
+                date: submit.date,
+                phone: submit.phone,
+                study: submit.study,
+                country: submit.country,
+                state: submit.state,
+                zip: submit.zip,
                 link: submit.link
-                }
+            }
             const exists = await userprifile.findOne(query);
             if (exists) {
                 return res.send({ success: false, submit: exists })
@@ -63,6 +105,61 @@ async function run() {
             return res.send({ success: true, result });
         })
 
+        //product
+        app.post('/product-post', async (req, res) => {
+            const productPost = req.body
+            const result = await product.insertOne(productPost);
+            res.send({ success: 'Added Product Successfully' })
+        })
+        app.get('/product-get', async (req, res) => {
+            const query = {}
+            const cursor = product.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+        app.delete('/product-del/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await product.deleteOne(query)
+            res.send(result)
+        })
+        //user
+        app.get('/user', async (req, res) => {
+            const users = await userlogin.find().toArray();
+            res.send(users);
+          });
+      
+          app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = await userlogin.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin })
+          })
+      
+          
+          app.put('/user/admin/:email', verifyJWT,async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const updateDoc = {
+              $set: { role: 'admin' },
+            };
+            const result = await userlogin.updateOne(filter, updateDoc);
+            res.send(result);
+          })
+          
+          //login time
+          app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+              $set: user,
+            };
+            const result = await userlogin.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.VALID_TOKEN, { expiresIn: '1h' })
+            res.send({ result, token });
+          });
     }
     finally {
         //await client.close()
